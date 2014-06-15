6fd18a7528c850ea5db8db3d52c9e1101ecf1a0c
tomo-e <tomoe.ak@gmail.com>
2014-06-16 01:05:21 +0900

    リストを出力

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(function() {
-  //var posts = new PostCollection();
+  // var posts = new PostCollection();
 
   var postview = new PostView();
 
 
-  // console.log(postview);
   // posts.fetch({
   //   success: function() {
   //     _.each(posts.models, function(post){
   //       var test = post.get('title');
   //       console.log(test);
+  //       console.log(posts.models);
   //     });
   //   }
   // });
--- js/view.js
+++ js/view.js
@@ -3,13 +3,16 @@ var PostView = Backbone.View.extend({
   collection: new PostCollection(),
   initialize: function() {
     var self = this;
+    var posts = new PostCollection();
+
     self.collection.fetch({
       success: function() {
-        _.each(self.collection, function(model) {
-
-          console.log(self.collection);
-          console.log(model);
-
+        _.each(self.collection.models, function(model) {
+            var title = model.get('title');
+            var $li = $('<li>').html(
+              model.get('title')
+            );
+            $('#post_list1').append($li);
         });
       }
     });
